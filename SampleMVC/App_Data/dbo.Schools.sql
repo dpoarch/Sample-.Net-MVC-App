c778813cd50cd4bb8c588035fb61b168151d0215
dpoarch <davidpoarch@outlook.com>
2018-02-06 17:17:11 +0800

    Initial Commit

---
+++ SampleMVC/App_Data/dbo.Schools.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Schools]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SchoolName] VARCHAR(50) NOT NULL, 
+    [Address] VARCHAR(MAX) NOT NULL, 
+    [Founder] VARCHAR(50) NOT NULL
+)
